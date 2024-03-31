7eb16641e7e36c7a164a5e2f5e2c45bcaac96995
alireza-fa <alirezafeyze44@gmail.com>
2024-03-31 11:29:47 +0330

    update finance models, add payment and gateway to django admin

--- apps/finance/admin.py
+++ apps/finance/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.finance.models import Gateway, Payment
+
+
+@admin.register(Gateway)
+class GatewayAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    pass
