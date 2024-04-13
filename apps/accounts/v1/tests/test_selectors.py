f9786e3729b3cb9aa1542e193805d0c3dc5ffba7
alireza-fa <alirezafeyze44@gmail.com>
2024-04-13 14:55:11 +0330

    apps/accounts selectors tests

---
+++ apps/accounts/v1/tests/test_selectors.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+from apps.accounts.models import BaseUser
+from apps.accounts.v1.selectors.base_user import get_user_by_phone_number, get_user_by_username
+from apps.accounts.v1.selectors.profile import get_profile_user
+
+
+class TestSelectors(TestCase):
+    def setUp(self):
+        self.user = BaseUser.objects.create_user(username="alireza", phone_number="09129121111")
+
+    def test_get_user_by_phone_number(self):
+        user = get_user_by_phone_number(phone_number=self.user.phone_number)
+        self.assertEqual(user, self.user)
+
+    def test_get_user_by_username(self):
+        user = get_user_by_username(username=self.user.username)
+        self.assertEqual(user, self.user)
+
+    def test_get_profile_user(self):
+        profile = get_profile_user(user=self.user)
+        self.assertIsNotNone(profile)
+        self.assertEqual(profile, self.user)
